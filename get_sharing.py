86e467f30ee01da6c2666359c91ac8e7df3765dd
finepix <499768985@qq.com>
2018-08-09 13:20:58 +0800

    bug fix and add filter to gofun.

--- get_sharing.py
+++ get_sharing.py
@@ -2,14 +2,19 @@ import itchat
 import itchat.content as ct
 from wechat_voucher import open_url_shouqiev as ous
 
+domain = "api.shouqiev.com"                 # 用来判断是否为gofun的券
+
 
 @itchat.msg_register(ct.INCOME_MSG)
 def msg_rq(msg):
-    if msg.MsgType == 1:                # 普通的文字
-        print(msg.text)
-    elif msg.MsgType == 49:             # 网页链接
-        print(msg.Url)
-        ous.open_url(msg.Url)           # 打开该链接
+    if hasattr(msg, 'MsgType'):
+        if msg.MsgType == 1:                # 普通的文字
+            print(msg.text)
+        elif msg.MsgType == 49:             # 网页链接
+            print(msg.Url)
+            url = msg.Url
+            if domain in url:               # 拉取gofun的链接
+                ous.open_url(msg.Url)       # 打开该链接
     return
 
 
